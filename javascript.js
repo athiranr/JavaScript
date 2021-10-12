3a5117ef2c35b6e09486c10ac23c463190a61c06
athiranr <athiranravi1995@gmail.com>
2021-10-11 21:17:56 -0700

    js file modified

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 console.log("hey");
+//functions
+function myFunc()
+{
+  let a = "Todays time up";
+  console.log(a);
+}
+
+
+function myFunc1()
+{
+    let data = "Flycatch infotech pvt.Kochin";
+    console.log(data);
+
+}
+myFunc1();
+myFunc();
