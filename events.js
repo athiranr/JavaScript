51deff4c6f8e33a29f759698d757ec2c631162da
athiranr <athiranravi1995@gmail.com>
2021-10-12 23:38:55 -0700

    event file is added

---
+++ events.js
@@ -0,0 +1,15 @@
+console.log("Events");
+// function onAlert()
+// {
+//    alert("you have clicked on the button");
+// }
+// let btn = document.getElementById("btn");
+// addEventListener("click",function(){
+//     alert("Clicked");
+// });
+let btn = document.getElementById("btn");
+addEventListener("click",myFunction);
+
+ function myFunction(){
+     alert("clicked");
+ }
